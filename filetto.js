3c84c645258affb5b8ff7a45133c8afbc0366a81
RickySanto <santoni_rik@hotmail.com>
2018-11-17 15:53:56 +0000

    Improved UI

--- filetto.js
+++ filetto.js
@@ -3,3 +3,13 @@
 // ctx.beginPath();
 // ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 // ctx.stroke();
+
+var cswitch = document.querySelector("input");
+var onLabel = document.querySelector("#on");
+var offLabel = document.querySelector("#off");
+
+
+cswitch.addEventListener ("click", function(){
+    onLabel.classList.toggle('notDisplay');
+    offLabel.classList.toggle('notDisplay');
+});
