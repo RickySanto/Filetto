42932b7e167330e18c7cb2c0e4aada792fe3e199
RickySanto <santoni_rik@hotmail.com>
2018-11-21 08:41:55 +0000

    Adding JS

--- filetto.js
+++ filetto.js
@@ -4,12 +4,36 @@
 // ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 // ctx.stroke();
 
-var cswitch = document.querySelector("input");
+var cswitch = document.getElementById("playComputer");
+var playXorO = document.getElementById("playXorO");
 var onLabel = document.querySelector("#on");
 var offLabel = document.querySelector("#off");
+var playButton = document.getElementById("play");
+
+var player1 = {
+    manual: true,
+    playing: 'x'
+}
+
+var player1 = {
+    manual: computer,
+    playing: 'o'
+}
+
 
 
 cswitch.addEventListener ("click", function(){
     onLabel.classList.toggle('notDisplay');
     offLabel.classList.toggle('notDisplay');
+
+ 
 });
+
+if (playComputer.checked === true ){
+    
+}
+
+if (playXorO.checked === true ){
+    
+}
+
